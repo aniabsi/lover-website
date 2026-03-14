2b7d1dfc2ea956b677938a43e77b168636aa5301
aniabsi <174658925+aniabsi@users.noreply.github.com>
2026-03-14 19:52:11 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Adem",
+    valentineName: "Adem 💗",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -42,15 +42,15 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "J'ai extrêmement hâte !!",  // Shows when they go past 5000%
-        high: "J'ai hâte !!",              // Shows when they go past 1000%
-        normal: "Ce sera cool"                           // Shows when they go past 100%
+        extreme: "J'ai extrêmement hâte !!",  // Shows when they go past 1000%
+        high: "J'ai hâte !!",              // Shows when they go past 100%
+        normal: "Ce sera cool"                           // Shows when they go past 50%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! Je suis la femme la plus chanceuse sur cette terre 💝💖💝💓",
-        message: "Maintenant je t'offre plein de calins et de bisous en récompense !",
+        message: "Maintenant je t'offre plein de calins en récompense !",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://open.spotify.com/intl-fr/track/1dGr1c8CrMLDpV6mPbImSI?si=d4d814343399401f", // Music streaming URL
+        musicUrl: "https://collection.cloudinary.com/dm1hhay8w/e77660666aa5c214fcbeab11f4df68fd", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
